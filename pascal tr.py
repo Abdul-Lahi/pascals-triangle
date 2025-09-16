e8e8005b034b7f34fd9817866c3baba95b46011e
Abdul-Lahi <adesinaabdulahi00@gmail.com>
2025-09-16 22:57:28 +0100

    Update pascal tr.py

--- pascal tr.py
+++ pascal tr.py
@@ -1,13 +1,3 @@
-'''fib_numbers = [1]
-sequence = int(input('Enter number of sequence: '))
-for x in range(1, sequence):
-    if len(fib_numbers) == 1:
-        fib_numbers.append(2)
-    else:
-        fib_numbers.append(fib_numbers[x-1]  + fib_numbers[x-2])
-print(fib_numbers)'''
-
-
 pascals_T = [[1]]
 sequence = int(input('Enter number of sequence: '))
 for x in range(1, sequence):
@@ -19,4 +9,5 @@ for x in range(1, sequence):
             newlist.append(pascals_T[x-1][y-1] + pascals_T[x-1][y])
         newlist.append(1)
         pascals_T.append(newlist)
+
 print(pascals_T).split()
